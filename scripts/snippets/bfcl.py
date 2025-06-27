5b0d15504cbcace1c5a92a05a1281acf96ac1a4d
Marta <martas@nvidia.com>
2025-06-27 08:52:36 +0200

    update snippets:
    * imports
    * formatting
    * unify checkpoints path in all examples
    * use the correct tokenizer
    * production endpoint for safety judge
    * 25.07 docker tag
    * put deploy inside `if __name__ == "__main__"`
    * use deploy.py in the readme
    
    Signed-off-by: Marta <martas@nvidia.com>

--- scripts/snippets/bfcl.py
+++ scripts/snippets/bfcl.py
@@ -3,8 +3,8 @@
 ## Export the required variables
 # No environment variables are required
 ## Run the evaluation
-from nemo.collections.llm import api
-from nemo.collections.llm.evaluation.api import EvaluationConfig, EvaluationTarget
+from nemo_eval.api import evaluate
+from nemo_eval.utils.api import EvaluationConfig, EvaluationTarget
 
 
 model_name = "triton_model"
@@ -13,18 +13,14 @@ chat_url = "http://0.0.0.0:8886/v1/chat/completions/"
 
 target_config = EvaluationTarget(
     api_endpoint={
-        "url": chat_url, 
+        "url": chat_url,
         "type": "chat",
-        }
-    )
-eval_config = EvaluationConfig(
-    type="bfclv3_ast",
-    output_dir="/results/",
-    params={"limit_samples": 10}
+    }
 )
+eval_config = EvaluationConfig(type="bfclv3_ast", output_dir="/results/", params={"limit_samples": 10})
 
 
-results = api.evaluate(target_cfg=target_config, eval_cfg=eval_config)
+results = evaluate(target_cfg=target_config, eval_cfg=eval_config)
 
 
 print(results)
--- scripts/snippets/simple_evals.py
+++ scripts/snippets/simple_evals.py
@@ -4,8 +4,8 @@
 ## Key with access to https://build.nvidia.com/ endpoints
 # export JUDGE_API_KEY=...
 ## Run the evaluation
-from nemo.collections.llm import api
-from nemo.collections.llm.evaluation.api import EvaluationConfig, EvaluationTarget
+from nemo_eval.api import evaluate
+from nemo_eval.utils.api import EvaluationConfig, EvaluationTarget
 
 
 model_name = "triton_model"
@@ -14,10 +14,10 @@ chat_url = "http://0.0.0.0:8886/v1/chat/completions/"
 
 target_config = EvaluationTarget(
     api_endpoint={
-        "url": chat_url, 
+        "url": chat_url,
         "type": "chat",
-        }
-    )
+    }
+)
 eval_config = EvaluationConfig(
     type="AIME_2025",
     output_dir="/results/",
@@ -25,7 +25,7 @@ eval_config = EvaluationConfig(
 )
 
 
-results = api.evaluate(target_cfg=target_config, eval_cfg=eval_config)
+results = evaluate(target_cfg=target_config, eval_cfg=eval_config)
 
 
 print(results)
