7b0eee7ec518c86d05ca43ab50fed8161ae72bc8
Abhishree <abhishreetm@gmail.com>
2025-07-14 13:47:24 -0700

    Fix linting error 2
    Signed-off-by: Abhishree <abhishreetm@gmail.com>

--- scripts/evaluation_with_nemo_run.py
+++ scripts/evaluation_with_nemo_run.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2025, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,7 +25,6 @@ import nemo_run as run
 
 from nemo_eval.api import deploy, evaluate
 from nemo_eval.utils.api import ApiEndpoint, ConfigParams, EvaluationConfig, EvaluationTarget
-
 
 ENDPOINT_TYPES = {"chat": "chat/completions/", "completions": "completions/"}
 
