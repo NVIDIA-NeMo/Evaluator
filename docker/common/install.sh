7043c50fa8eb51c9f4b7ad4760768e42b14f8e8e
oliver könig <okoenig@nvidia.com>
2025-09-03 11:43:24 +0000

    test
    Signed-off-by: oliver könig <okoenig@nvidia.com>

--- docker/common/install.sh
+++ docker/common/install.sh
@@ -22,24 +22,36 @@ while [[ $# -gt 0 ]]; do
         PACKAGE="$2"
         shift 2
         ;;
+    --python-version)
+        PYTHON_VERSION="$2"
+        shift 2
+        ;;
+    --use-uv)
+        USE_UV="true"
+        shift 1
+        ;;
     *)
         echo "Unknown option: $1"
-        echo "Usage: $0 --base-image {pytorch|cuda}"
+        echo "Usage: $0 --package {nemo-evaluator} --python-version {3.10|3.11|3.12}"
         exit 1
         ;;
     esac
 done
 
-if [[ -z "${PACKAGE:-}" ]]; then
+if [[ -z "${PACKAGE:-}" || -z "${PYTHON_VERSION:-}" ]]; then
     echo "Error: --package argument is required"
-    echo "Usage: $0 --package {nemo-evaluator}"
+    echo "Usage: $0 --package {nemo-evaluator} --python-version {3.10|3.11|3.12}"
     exit 1
 fi
 
-if [[ "$PACKAGE" != "nemo-evaluator" ]]; then
-    echo "Error: --package must be either 'nemo-evaluator'"
-    echo "Usage: $0 --package {nemo-evaluator}"
+if [[ "$PACKAGE" != "nemo-evaluator" || ("$PYTHON_VERSION" != "3.10" && "$PYTHON_VERSION" != "3.11" && "$PYTHON_VERSION" != "3.12") ]]; then
+    echo "Error: --package must be either 'nemo-evaluator' and --python-version must be either '3.10', '3.11', or '3.12'"
+    echo "Usage: $0 --package {nemo-evaluator} --python-version {3.10|3.11|3.12}"
     exit 1
+fi
+
+if [[ -z "${USE_UV:-}" ]]; then
+    USE_UV="false"
 fi
 
 
@@ -55,36 +67,39 @@ main() {
     apt-get install -y curl
 
     # Install Python
-    export PYTHON_VERSION=3.12
     apt-get update
     apt-get install -y software-properties-common
     add-apt-repository ppa:deadsnakes/ppa -y
     apt-get install -y python$PYTHON_VERSION-dev python$PYTHON_VERSION-venv
     update-alternatives --install /usr/bin/python3 python3 /usr/bin/python$PYTHON_VERSION 1
 
-    # Install uv
-    UV_VERSION="0.7.2"
-    curl -LsSf https://astral.sh/uv/${UV_VERSION}/install.sh | sh
-    export PATH="$HOME/.local/bin:$PATH"
-    export UV_PROJECT_ENVIRONMENT=/opt/venv
-    export PATH="$UV_PROJECT_ENVIRONMENT/bin:$PATH"
-    export UV_LINK_MODE=copy
-    export CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
-
-    # Create virtual environment and install dependencies
-    uv venv ${UV_PROJECT_ENVIRONMENT} $([[ "$BASE_IMAGE" == "pytorch" ]] && echo "--system-site-packages")
-
     cd packages/$PACKAGE
 
-    # Install dependencies
-    uv sync --locked --only-group build ${UV_ARGS[@]}
-    uv sync \
-        --link-mode copy \
-        --locked \
-        --all-groups ${UV_ARGS[@]}
+    if [[ "$USE_UV" == "true" ]]; then
+        # Install uv
+        UV_VERSION="0.7.2"
+        curl -LsSf https://astral.sh/uv/${UV_VERSION}/install.sh | sh
+        export PATH="$HOME/.local/bin:$PATH"
+        export UV_PROJECT_ENVIRONMENT=/opt/venv
+        export PATH="$UV_PROJECT_ENVIRONMENT/bin:$PATH"
+        export UV_LINK_MODE=copy
 
-    # Install the package
-    uv pip install --no-deps -e .
+        # Create virtual environment and install dependencies
+        uv venv ${UV_PROJECT_ENVIRONMENT} $([[ "$BASE_IMAGE" == "pytorch" ]] && echo "--system-site-packages")
+
+        # Install dependencies
+        uv sync --locked --only-group build ${UV_ARGS[@]}
+        uv sync \
+            --link-mode copy \
+            --locked \
+            --all-groups ${UV_ARGS[@]}
+
+        # Install the package
+        uv pip install --no-deps -e .
+    else
+        pip install -e .
+    fi
+
 
 }
 
