12ff32f57e45d87ceb0637bd218137c4ab1367c5
Marta Stepniewska-Dziubinska <martas@nvidia.com>
2025-09-08 12:02:57 +0200

    bulk update from internal version
    Signed-off-by: Marta Stepniewska-Dziubinska <martas@nvidia.com>

--- packages/nemo-evaluator-launcher/src/nemo_evaluator_launcher/exporters/__init__.py
+++ packages/nemo-evaluator-launcher/src/nemo_evaluator_launcher/exporters/__init__.py
@@ -1,18 +1,18 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 """Exporter registration and factory."""
 
 from nemo_evaluator_launcher.exporters.gsheets import GSheetsExporter
@@ -27,11 +27,10 @@ def create_exporter(name: str, config: dict = None):
 
 
 __all__ = [
-    "create_exporter",
-    "available_exporters",
-    "get_exporter",
     "GSheetsExporter",
     "LocalExporter",
     "MLflowExporter",
     "WandBExporter",
+    "available_exporters",
+    "get_exporter",
 ]
