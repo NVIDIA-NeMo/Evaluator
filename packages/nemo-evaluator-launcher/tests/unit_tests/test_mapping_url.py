c5b595e1ec6615d726831a8f28e364048af46be1
Alex Gronskiy <agronskiy@nvidia.com>
2025-09-15 17:02:57 +0200

    chore: linting
    Signed-off-by: Alex Gronskiy <agronskiy@nvidia.com>

--- packages/nemo-evaluator-launcher/tests/unit_tests/test_mapping_url.py
+++ packages/nemo-evaluator-launcher/tests/unit_tests/test_mapping_url.py
@@ -15,17 +15,9 @@
 #
 """Tests for mapping URL validation."""
 
-import os
 import sys
 
-import pytest
-
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import tomli as tomllib
-
-from nemo_evaluator_launcher.common.mapping import MAPPING_URL, _download_latest_mapping
+from nemo_evaluator_launcher.common.mapping import MAPPING_URL
 
 
 def test_mapping_url_contains_main():
