4817b097fd686e03476cba7a1b378bbe86cb3563
Wojciech Prazuch <wprazuch@nvidia.com>
2026-05-14 12:42:11 +0200

    chore: add SPDX header to test_interceptors_extended.py
    Signed-off-by: Wojciech Prazuch <wprazuch@nvidia.com>

--- tests/test_adapters/test_interceptors_extended.py
+++ tests/test_adapters/test_interceptors_extended.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Tests for interceptors not covered by test_interceptors.py."""
 
 from __future__ import annotations
