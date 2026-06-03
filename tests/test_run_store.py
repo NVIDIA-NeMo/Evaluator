f65f81c796c28a409e3c65808e74312f2169641b
Rita Fernandes Neves <rfernandesne@nvidia.com>
2026-06-03 10:56:17 +0100

    test: add missing SPDX header
    Signed-off-by: Rita Fernandes Neves <rfernandesne@nvidia.com>

--- tests/test_run_store.py
+++ tests/test_run_store.py
@@ -1,3 +1,12 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
 from __future__ import annotations
 
 from unittest.mock import patch
